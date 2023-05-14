10386fb26e96aaee2627c13a1a8b19efc1910cb4
MohammedGhaly <medooghaly7@gmail.com>
2023-05-14 10:58:42 -0700

    solving task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+/**
+ * is_palindrome - chechks if the linked list is a plindrome or not
+ * @head: head node of the linked list
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slower = *head, *faster = *head;
+	listint_t *previous = NULL, *tmp;
+
+	while (faster != NULL && faster->next != NULL)
+	{
+		faster = faster->next->next;
+		tmp = slower;
+		slower = slower->next;
+		tmp->next = previous;
+		previous = tmp;
+	}
+
+	if (faster)
+		slower = slower->next;
+	while (slower != NULL)
+	{
+		if (previous->n != slower->n)
+			return (0);
+		previous = previous->next;
+		slower = slower->next;
+	}
+	return (1);
+}
