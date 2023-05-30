4a24b1b6bb7eced0755da13a6edc7672d79ed218
MohammedGhaly <medooghaly7@gmail.com>
2023-05-30 09:41:22 -0700

    fixing pycode

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,6 +79,6 @@ class Square:
         elif isinstance(value[1], int) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")    
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
