4c2ec556467d3671cb0a2457720de18da741651c
MohammedGhaly <medooghaly7@gmail.com>
2023-06-20 13:29:22 -0700

    solving tasks [5, 6]

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+Rectangle = __import__('6-rectangle').Rectangle
 
-Rectangle = __import__('4-rectangle').Rectangle
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
 
-my_rectangle = Rectangle(2, 4)
-print(str(my_rectangle))
-print("--")
-print(my_rectangle)
-print("--")
-print(repr(my_rectangle))
-print("--")
-print(hex(id(my_rectangle)))
