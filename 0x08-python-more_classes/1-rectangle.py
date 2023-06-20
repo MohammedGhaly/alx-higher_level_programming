e70836789ea5e67f5c9168f7ef488cb71460273b
MohammedGhaly <medooghaly7@gmail.com>
2023-06-20 12:31:35 -0700

    fixing indent

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         """width setter"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
-         if value < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
