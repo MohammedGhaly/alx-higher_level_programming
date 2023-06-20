924ccc6744283ef8a4a4d6d49e7c102d70b48204
MohammedGhaly <medooghaly7@gmail.com>
2023-06-20 12:27:35 -0700

    solving tasks [0, 1]

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module containing Rectangle class
+"""
+
+
+class Rectangle:
+    """class that represents a Rectangle shape"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+module containing Rectangle class
+"""
+
+
+class Rectangle:
+    """class that represents a Rectangle shape"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
