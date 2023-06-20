d307dc1b4d43390066ab94c00fc9ec9620099438
MohammedGhaly <medooghaly7@gmail.com>
2023-06-20 12:43:31 -0700

    fixing init to use setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,22 +9,25 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''default init to initialize instances'''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         '''a property to return the value of width'''
+
         return self.__width
 
     @property
     def height(self):
         '''a property to return the value of height'''
+
         return self.__height
 
     @width.setter
     def width(self, value):
         """width setter"""
+
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
@@ -34,6 +37,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """height setter"""
+
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
