05bcaf98d4552c3a2b0dfbc03be111517eea1b02
MohammedGhaly <medooghaly7@gmail.com>
2023-06-25 04:03:28 -0700

    fixing documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''
+"""
 contains 'MyList' class
-'''
+"""
 
 
 class MyList(list):
-    '''
+    """
     class that inherits from list
-    '''
+    """
 
     def print_sorted(self):
-        '''
+        """
         sorts a list of integers
-        '''
+        """
 
         print(sorted(self))
