9ac404375920a363e4c850318a56f31efd2d7a54
MohammedGhaly <medooghaly7@gmail.com>
2023-06-25 15:28:25 -0700

    solving tasks 5, 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains 'BaseGeometry' class
+"""
+
+
+class BaseGeometry:
+    """
+    class that represents a basic geometric shape
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains 'BaseGeometry' class
+"""
+
+
+class BaseGeometry:
+    """
+    class that represents a basic geometric shape
+    """
+
+    def area(self):
+        """
+        calculates the area of the geometric shape if it is valid
+        raises an Exception otherwise
+        """
+        raise Exception('area() is not implemented')
