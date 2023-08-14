c64a15fe1db22c62b1103310301c904d1d283c20
MohammedGhaly <medooghaly7@gmail.com>
2023-08-14 21:34:37 +0000

    tasks 6, 7 solved

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+first = process.argv[2];
+second = process.argv[3];
+console.log(first + ' is ' + second);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+const arr = [c, p, j];
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
