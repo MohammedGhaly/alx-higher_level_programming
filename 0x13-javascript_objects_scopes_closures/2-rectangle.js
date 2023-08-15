2c28f42c88c0a33363ba78a81f5c9425754875ee
MohammedGhaly <medooghaly7@gmail.com>
2023-08-15 19:04:43 +0300

    semistandard style fixed for 2&3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (!(w == undefined || h == undefined || w <= 0 || h <= 0)) {
+    if (!(w === undefined || h === undefined || w <= 0 || h <= 0)) {
       this.width = w;
       this.height = h;
     }
