cb66d46c2f014bf5e4673aad5395d9d863ba3bba
MohammedGhaly <medooghaly7@gmail.com>
2023-08-15 19:45:43 +0300

    task 6 really fixed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,17 +6,11 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-      let s = ''
-      let h;
-      if (c === undefined) {h = 'X'}
-      else {
-        h = c;
-        for(let i = 0; i < this.width; i++)
-          s += h;
-        for(let i = 0; i < this.height; i++)
-          console.log(s);
-      }
-    }
+    let s = '';
+    let h;
+    if (c === undefined) { h = 'X'; } else { h = c; }
+    for (let i = 0; i < this.width; i++) { s += h; }
+    for (let i = 0; i < this.height; i++) { console.log(s); }
   }
 }
 module.exports = Square;
