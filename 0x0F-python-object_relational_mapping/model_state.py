9abc24fcafd9bbc441ff903951024d4eeb586dec
MohammedGhaly <medooghaly7@gmail.com>
2023-08-19 09:05:52 -0700

    State class created

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+'''class definition of a State and an instance Base = declarative_base()'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declerative_base()
+
+
+class State(Base):
+    '''a class that represents a state'''
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
