58a29848a40366151a5e79171398f15c6e036a6f
MohammedGhaly <medooghaly7@gmail.com>
2023-08-19 09:16:16 -0700

    changing parameters order

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -21,5 +21,5 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
