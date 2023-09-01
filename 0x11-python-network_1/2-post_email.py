475555e91eb7169cd545206bf9080b99e8314ae1
MohammedGhaly <medooghaly7@gmail.com>
2023-09-01 12:51:31 -0700

    task 2 solved

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+ a Python script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and displays the body.
+'''
+
+
+import urllib.request as uq
+import urllib.parse as up
+import sys
+
+
+url, email = (sys.argv[1], sys.argv[2])
+data = {'email': email}
+data = up.urlencode(data)
+data = data.encode('ascii')
+
+req = uq.Request(url, data)
+with uq.urlopen(req) as res:
+    print(res.read().decode('utf8'))
