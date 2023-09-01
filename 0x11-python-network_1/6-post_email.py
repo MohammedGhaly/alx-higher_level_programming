ca4f21c1b8896a03ef732f6b6406173b3a2b3b83
MohammedGhaly <medooghaly7@gmail.com>
2023-09-01 15:30:04 -0700

    task 6 solved

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 '''
-
+script that takes in a URL and an email address, sends a POST request
+with the email as a parameter, and finally displays the body of the response.
 '''
+import requests as rq
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    headers = {'email', sys.argv[2]}
+    r = rq.post(url, headers)
+    print(r.text)
