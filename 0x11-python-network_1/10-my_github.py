d1a08b1ba85ffa3fdde4eb62122cc3858a7608f2
MohammedGhaly <medooghaly7@gmail.com>
2023-09-01 16:54:10 -0700

    task 10 solved

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ import sys
 
 if __name__ == '__main__':
     url = 'https://api.github.com/user'
-    r = get(url, auth=(argv[1], argv[2]))
+    r = get(url, auth=(sys.argv[1], sys.argv[2]))
     print(r.json().get('id'))
