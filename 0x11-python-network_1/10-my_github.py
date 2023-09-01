ad3d9c300a31702961a61be4e5c9889059d84b7e
MohammedGhaly <medooghaly7@gmail.com>
2023-09-01 16:50:25 -0700

    task 10 fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,8 @@ a script that takes GitHub credentials to display your id
 import requests
 import sys
 
+
+if __name__ == '__main__':
 url = 'https://api.github.com/user'
 r = get(url, auth=(argv[1], argv[2]))
     print(r.json().get('id'))
