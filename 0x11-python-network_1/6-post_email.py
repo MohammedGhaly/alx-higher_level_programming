fd6a4a0feec5a12c4b4730466d95fa0236febc27
MohammedGhaly <medooghaly7@gmail.com>
2023-09-01 15:55:49 -0700

    task 6 solved

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """script that posts an email in an http request"""
-import sys
 import requests
+import sys
 
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    headers = {'email', sys.argv[2]}
-    r = requests.post(url, data=headers)
-    print(r.text)
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    res = requests.post(sys.argv[1], data=email)
+    print(res.text)
