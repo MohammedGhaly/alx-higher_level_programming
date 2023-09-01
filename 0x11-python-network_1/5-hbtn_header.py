28a0f7befda00949ba5052e33585e886ed9081d0
MohammedGhaly <medooghaly7@gmail.com>
2023-09-01 15:23:06 -0700

    refactored task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 '''
-script that takes in a URL, sends a request to the URL and displays
-the value of the variable X-Request-Id in the response header
+    script that takes in a URL, sends a request to the URL and displays
+    the value of the variable X-Request-Id in the response header
 '''
-
-
-import requests as rq
+import requests
 import sys
 
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    r = rq.get(url)
+    r = requests.get(sys.argv[1])
     print(r.headers['X-Request-Id'])
